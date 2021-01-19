acaa508c657b255716a7c817437d76273ef08a99
unknown <cain.mcdonald@my.jcu.edu.au>
2021-01-19 16:46:13 +1000

    add dependencies

--- src/messages.js
+++ src/messages.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function messages() {
-  return <div></div>;
+function Messages() {
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const [items, setItems] = useState([]);
+
+  const fetchItems = async () => {
+    const data = await fetch(
+      "https://fortnite-api.theapinetwork.com/upcoming/get"
+    );
+
+    const items = await data.json();
+    console.log(items.items);
+    setItems(items.items);
+  };
+
+  return (
+    <div>
+      {items.map((item) => (
+        <h1>{item.name}</h1>
+      ))}
+    </div>
+  );
 }
 
-export default messages;
+export default Messages;
